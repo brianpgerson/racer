73e45c20d365b8fdbdf383867fa5ff32258c861c
Brian Gerson <brianpgerson@gmail.com>
2015-07-08 10:32:47 -0700

    added finish line + winner declaration enforced single responsibility by creating a checkForFinish function

--- racer.js
+++ racer.js
@@ -1,11 +1,24 @@
-document.addEventListener('keypress', getCharCode);
+document.addEventListener('keypress', updatePlayerPosition);
 
 var p1 = 0;
 var p2 = 0;
+var finish = 5;
 
-function getCharCode(event){
+function updatePlayerPosition(event){
 	if (event.charCode == 112){
 		p1++;
+		checkForFinish(p1, "Papa John");
 		console.log(p1);
+	} else if (event.charCode == 113){
+		p2++;
+		checkForFinish(p2, "Quiznos");
+		console.log(p2);
+	}
+
+}
+
+function checkForFinish(position, player){
+	if (finish - position == 0){
+		alert(player + " won!");
 	}
 }
