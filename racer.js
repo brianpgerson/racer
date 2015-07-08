54e2aafb8adf27fff4413314d2071e643efe0896
Brian Gerson <brianpgerson@gmail.com>
2015-07-08 10:23:44 -0700

    created event listener and player position store

--- racer.js
+++ racer.js
@@ -1,1 +1,11 @@
-console.log("HI");
+document.addEventListener('keypress', getCharCode);
+
+var p1 = 0;
+var p2 = 0;
+
+function getCharCode(event){
+	if (event.charCode == 112){
+		p1++;
+		console.log(p1);
+	}
+}
