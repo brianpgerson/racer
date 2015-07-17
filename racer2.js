16803bb380b26cae47886e915e3bb73410bdf5b2
Brian Gerson <brianpgerson@gmail.com>
2015-07-16 18:00:47 -0700

    learned things wow

--- racer2.js
+++ racer2.js
@@ -1,4 +1,38 @@
-var charCodes = [112, 113];
+window.addEventListener('load', init);
+
+function init() {
+	var playersDropDown = document.getElementById("playersDropDown");
+	playersDropDown.addEventListener("change", createInputFields, false);
+}
+
+
+function resetInputField(nodeList){
+	while (nodeList.hasChildNodes()) {
+		nodeList.removeChild(nodeList.firstChild);
+	}
+}
+
+function createInputFields(event, value){
+	var keyInputFields = document.getElementById("playerKeys");
+	var howManyFields = event.target.selectedIndex;
+	resetInputField(keyInputFields);
+	for (i=0; i < howManyFields; i++){
+		var fieldHTML = "<br /><label for='player"+i+"'>Key for Player "+(i+1)+":</label><input type='text' class='player'><br />";
+		keyInputFields.innerHTML += fieldHTML;
+	}
+}
+
+
+function getCharacters(){
+	var nodeList = document.querySelectorAll('.player');
+	var charArray = [];
+	for (i=0; i<nodeList; i++){
+		charArray.push(nodeList[i].value);
+	}
+	return charArray;
+}
+
+
 
 function playerCreate(array){
 	return array.map(function(currentValue, index){
@@ -11,6 +45,3 @@ function Player(id, charCode){
 	this.position = 0;
 	this.charCode = charCode;	
 }
-
-var players = playerCreate(charCodes);
-console.log(players);
